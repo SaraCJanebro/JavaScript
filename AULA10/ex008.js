6b89df5bc66c3f95a77336600ac74c9b8489af94
Sara Camurça Janebro <saracj@gmail.com>
2022-10-13 20:55:19 +0100

    Create ex008.js
    if

---
+++ AULA10/ex008.js
@@ -0,0 +1,11 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+console.log(`Dirija sempre com cinto de segurança!`)
+console.log(`Você foi multado`)
+
+var vel = 12 
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel>60)  {
+    console.log (`Você foi muktado!`)
+}
+    console.log(`Dirija sempre usando cinto de segurança`)
