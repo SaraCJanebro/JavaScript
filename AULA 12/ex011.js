febb9615d32cfc931f885d1703f183dd07af8676
Sara Camurça Janebro <saracj@gmail.com>
2022-12-12 20:43:52 +0000

    js

--- AULA 12/ex011.js
+++ AULA 12/ex011.js
@@ -4,3 +4,12 @@ if(idade < 18) {
 } else {
     console.log ('Maior de idade')
 }
+
+var idade = 22
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade <18) || idade > 65 {
+    console.log ('Voto opcional')
+ } else { 
+    console.log('Voto obrogatório')
+ }
